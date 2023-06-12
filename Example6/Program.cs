5bfcab524e59d1343f1209bc4bd7bba692768902
Nikolay Karasev <nomotorolla@gmail.com>
2023-06-12 23:50:19 +0300

    create new folder Example6, add solved example

---
+++ Example6/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка). */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
